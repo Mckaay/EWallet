f660d6def6b86b27ec18cd835023fdc7f6aad89f
Michal Nycz <michal1nycz@gmail.com>
2024-08-09 13:32:17 +0200

    Project Setup

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use App\Models\User;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use LazilyRefreshDatabase;
+
+
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_if_user_can_visit_homepage(): void
+    {
+        $sampleUser = User::factory()->create();
+        $response = $this->actingAs($sampleUser)->get('/');
+        $response->assertStatus(200);
+    }
+}
